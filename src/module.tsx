0a6c72656d1e896254e61cf3628bde1b7aef0cec
stefan <stefan.surzycki@gmail.com>
2020-03-04 11:22:02 +0100

    jacked up

--- src/module.tsx
+++ src/module.tsx
@@ -3,7 +3,7 @@ import './css/panel.light.scss';
 
 import { PanelProps, PanelPlugin } from '@grafana/data';
 import React, { PureComponent } from 'react';
-import { Canvas } from 'react-three-fiber'
+import * as THREE from "three";
 
 // TODO: make styles local for panel
 //loadPluginCss({
@@ -14,12 +14,32 @@ import { Canvas } from 'react-three-fiber'
 export class ReactPanelTemplate extends PureComponent<PanelProps> {
   _panelPath: string;
 
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(200, 200);
+    document.getElementById("hello").appendChild(renderer.domElement);
+    //document.body.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }
+
   render() {
     return (
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-      </Canvas>
+      <div id="hello"/>
     );
   }
 
